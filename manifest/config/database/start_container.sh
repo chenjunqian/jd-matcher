7cf193ddbbe195f1ed3ed9ad0fb03df7178b6282
chenjunqian <chenjunqian0810@foxmail.com>
2024-11-24 00:50:04 +0800

    update: add local db container startup script

---
+++ manifest/config/database/start_container.sh
@@ -0,0 +1,9 @@
+docker run -d \
+  --name jdmatcher-postgres \
+  -e POSTGRES_USER=jdmatcher \
+  -e POSTGRES_PASSWORD=1qaz!QAZ \
+  -e POSTGRES_DB=jdmatcherdb \
+  -p 5432:5432 \
+  --net=host \
+  -v $(pwd)/db-data:/var/lib/postgresql/data \
+  groonga/pgroonga:3.2.4-alpine-17
